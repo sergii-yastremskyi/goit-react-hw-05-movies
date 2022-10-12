0c5efdc522e1a4255aa9aef19a6f1d838c5fcab8
sergii_yastremskyi <theyastremsky@gmail.com>
2022-10-12 18:57:48 +0300

    consoles cleared

--- src/SharedLayout/SharedLayout.jsx
+++ src/SharedLayout/SharedLayout.jsx
@@ -4,7 +4,6 @@ import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 export default function SharedLayout() {
   const navigate = useNavigate();
   const params = useParams();
-  console.log(params);
   useEffect(() => {
     if (params['*']) {
       navigate('/');
